7ac34be360778496fcd689577759b8ceeca6722a
Chuck Dries <chuck@chuckdries.com>
2021-10-17 10:01:50 -0700

    Clean up homepage cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,42 +52,43 @@ const Home: NextPage<Props> = (props) => {
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {props.stories.map((story, i) => (
-            <div className="filter rounded-lg shadow-2xl h-full w-full" key={i}>
+            <div
+              className="filter rounded-lg overflow-hidden shadow-2xl"
+              key={i}
+            >
               <a
-                className="h-full"
+                className="h-full flex flex-col"
                 href={story.link}
                 target="_blank"
                 rel="noreferrer"
               >
-                <div className="h-full">
-                  <div className="h-1/2 flex">
-                    {story?.media?.$?.url ? (
-                      <img
-                        className="h-full w-auto mx-auto"
-                        src={story?.media?.$?.url}
-                        alt="Story thumbnail"
-                      />
-                    ) : (
-                      <img
-                        className="h-full w-auto mx-auto"
-                        src="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i2oo9.m1w5KI/v2/piHJkQ_WoLj1E/-1x-1.png"
-                        alt="Thumbnail placeholder"
-                        style={{
-                          filter: "blur(2px)",
-                        }}
-                      />
-                    )}
-                  </div>
-
-                  <div className="p-4 relative h-1/2">
-                    <h6 className="text-lg md:text-xl lg:text-2xl">
-                      {story.title}
-                    </h6>
-                    <p className="text-sm text-gray-400 absolute bottom-3 right-3">
-                      {new Date(story.pubDate).toLocaleString()}
-                    </p>
-                  </div>
+                <div className="">
+                  {story?.media?.$?.url ? (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      className="object-cover"
+                      src={story?.media?.$?.url}
+                      alt="Story thumbnail"
+                    />
+                  ) : (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      className="object-cover"
+                      src="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i2oo9.m1w5KI/v2/piHJkQ_WoLj1E/-1x-1.png"
+                      alt="Thumbnail placeholder"
+                      style={{
+                        filter: "blur(2px)",
+                      }}
+                    />
+                  )}
                 </div>
+
+                <h6 className="text-lg md:text-xl lg:text-2xl p-4 flex-auto">
+                  {story.title}
+                </h6>
+                <p className="text-sm text-gray-400 self-end justify-self-end p-4 m-0">
+                  {new Date(story.pubDate).toLocaleString()}
+                </p>
               </a>
             </div>
           ))}
